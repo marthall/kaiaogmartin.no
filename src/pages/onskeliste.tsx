aabae298ef1777033486046dc465f5d43aa2bf9d
Martin Hallén <martin@rippel.run>
2024-02-04 17:36:32 +0100

    Release ønskeliste

--- src/pages/onskeliste.tsx
+++ src/pages/onskeliste.tsx
@@ -1,8 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
-import { signal } from "@preact/signals";
+import {
+  collection,
+  doc,
+  getFirestore,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
+import { effect, signal } from "@preact/signals";
 import { Nav } from "./nav.tsx";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,25 +27,67 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+const COLLECTION_NAME = "onskeliste";
+
 type Onske = {
   id: string;
   name: string;
   link: string;
   reserved: boolean;
+  sortKey: number;
+  userId?: string;
 };
-const onskelisteState = signal<Onske[]>([]);
 
-const docRef = collection(db, "onskeliste");
-
-async function fetchData() {
-  const docSnap = await getDocs(docRef);
-  onskelisteState.value = docSnap.docs.map((doc) => doc.data() as Onske);
+function generateId() {
+  return (
+    Math.random().toString(36).substring(2) + new Date().getTime().toString(36)
+  );
 }
 
-fetchData();
+const onskelisteState = signal<Onske[]>([]);
+
+const USER_ID_KEY = "userId";
+const currentUserId = signal<string>(
+  localStorage.getItem(USER_ID_KEY) ?? generateId(),
+);
+effect(() => {
+  window.localStorage.setItem(USER_ID_KEY, currentUserId.value);
+});
+
+const onskelisteCollectionRef = collection(db, COLLECTION_NAME);
+
+onSnapshot(onskelisteCollectionRef, (snapshot) => {
+  onskelisteState.value = snapshot.docs
+    .map((doc) => ({ ...doc.data(), id: doc.id }) as Onske)
+    .sort((a, b) => {
+      return a.sortKey - b.sortKey;
+    });
+});
+
+async function reserveOnske(id: string) {
+  console.log(id);
+  return await updateDoc(doc(db, COLLECTION_NAME, id), {
+    reserved: true,
+    userId: currentUserId.value,
+  });
+}
+
+async function angreOnske(id: string) {
+  console.log(id);
+  return await updateDoc(doc(db, COLLECTION_NAME, id), {
+    reserved: false,
+    userId: "",
+  });
+}
 
 // @ts-ignore
-const CAN_ADD = new URLSearchParams(window.location.search).has("admin");
+function resetAll() {
+  onskelisteState.value.forEach((onske) => {
+    updateDoc(doc(db, COLLECTION_NAME, onske.id), {
+      reserved: false,
+    });
+  });
+}
 
 export default function () {
   return (
@@ -46,10 +95,19 @@ export default function () {
       <Nav includeBack={true} />
       <div className={"center-container"}>
         <h1>Ønskeliste</h1>
-        <p className={"coming"}>Kommer</p>
+        <p>
+          Det absolutt viktigste for oss er at du kommer i bryllupet! Om du også
+          har lyst til å gi oss en gave er det veldig hyggelig. Under finner du
+          en liste over ting vi ønsker oss. Trykk på knappen der det står
+          "Reservér", så ser andre at du skal gi denne slik at vi ikke ender opp
+          med mange stekepanner.
+        </p>
+        <p>
+          Gjerne gå sammen flere om en gave, og det er også veldig hyggelig med
+          brukte ting som du tror vi kan ha glede av.
+        </p>
 
-        {/*<Onskeliste />*/}
-        {/*{CAN_ADD && <AddNew />}*/}
+        <Onskeliste />
       </div>
     </>
   );
@@ -58,73 +116,72 @@ export default function () {
 // @ts-ignore
 function Onskeliste() {
   return (
-    <div className={"onskeliste"}>
-      {onskelisteState.value.map((onske) => (
-        <OnskeItem key={onske.id} onske={onske} />
-      ))}
-    </div>
-  );
-}
-
-function OnskeItem({ onske }: { onske: Onske }) {
-  return (
     <>
-      <span>{onske.name}</span>
-      <a href={onske.link}>Til nettbutikk</a>
-      <button disabled={onske.reserved}>
-        {onske.reserved ? "Reservert" : "Reservér"}
-      </button>
+      <div className={"onskeliste"}>
+        {onskelisteState.value.map((onske) => (
+          <OnskeItem key={onske.id} onske={onske} />
+        ))}
+      </div>
+      {/*<button onClick={resetAll}>Reset</button>*/}
     </>
   );
 }
 
-const emptyOnske: Omit<Onske, "id"> = {
-  name: "",
-  link: "",
-  reserved: false,
-};
+function OnskeItem({ onske }: { onske: Onske }) {
+  const isLink = onske.link.startsWith("https");
 
-const newOnske = signal<Omit<Onske, "id">>(emptyOnske);
+  if (!isLink) {
+    return (
+      <>
+        <p className={"name"}>{onske.name}</p>
+        <p className={"description"}>{onske.link}</p>
+      </>
+    );
+  }
+  if (!onske.reserved) {
+    return (
+      <>
+        <a className={"name"}>{onske.name}</a>
+        <a className={"link"} href={onske.link} target={"_blank"}>
+          Nettbutikk
+        </a>
+        <button className={"reserve"} onClick={() => reserveOnske(onske.id)}>
+          Reservér
+        </button>
+      </>
+    );
+  }
 
-async function addOnske() {
-  await addDoc(docRef, newOnske.value);
-  await fetchData();
-  newOnske.value = emptyOnske;
-}
+  if (onske.userId && onske.userId === currentUserId.value) {
+    return (
+      <>
+        <p className={"name-reserved"}>{onske.name}</p>
+        <a className={"link"} href={onske.link} target={"_blank"}>
+          Nettbutikk
+        </a>
+        <button
+          className={"reserved-by-user"}
+          onClick={() => angreOnske(onske.id)}
+        >
+          Reservert av deg
+        </button>
+      </>
+    );
+  }
 
-// @ts-ignore
-function AddNew() {
   return (
-    <div className={"form"}>
-      <div>
-        <label htmlFor="name">Navn</label>
-        <input
-          key={"name"}
-          value={newOnske.value.name}
-          type="text"
-          onChange={(e) => {
-            newOnske.value = {
-              ...newOnske.value,
-              name: e.currentTarget.value,
-            };
-          }}
-        />
-      </div>
-      <div>
-        <label htmlFor="link">Lenke</label>
-        <input
-          value={newOnske.value.link}
-          type="text"
-          onChange={(e) => {
-            newOnske.value = {
-              ...newOnske.value,
-              link: e.currentTarget.value,
-            };
-          }}
-        />
-      </div>
-
-      <button onClick={addOnske}>Legg til</button>
-    </div>
+    <>
+      <p className={"name-reserved"}>{onske.name}</p>
+      <a className={"link"} href={onske.link} target={"_blank"}>
+        Nettbutikk
+      </a>
+      <button
+        className={"reserve"}
+        disabled={true}
+        onClick={() => reserveOnske(onske.id)}
+      >
+        Reservert
+      </button>
+    </>
   );
 }
